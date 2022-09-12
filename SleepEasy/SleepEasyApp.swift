5144561a09f77d555770fbab02ab5ad2356a56fd
Luka <lukasalipur.dev@gmail.com>
2022-09-12 14:29:44 +0200

    Initial Commit

---
+++ SleepEasy/SleepEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepEasyApp.swift
+//  SleepEasy
+//
+//  Created by Luka Šalipur on 12.9.22..
+//
+
+import SwiftUI
+
+@main
+struct SleepEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
